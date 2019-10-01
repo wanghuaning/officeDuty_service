866fea05173155b5b85a0b6922e3d3d4a3b696f5
whn <386011382@qq.com>
2019-10-01 12:43:28 +0800

    s

--- src/main/java/com/local/entity/sys/SYS_USER.java
+++ src/main/java/com/local/entity/sys/SYS_USER.java
@@ -6,6 +6,7 @@ import org.nutz.dao.entity.annotation.*;
 
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 @ApiModel("用户表")//用在模型类上，对模型类做注释；
@@ -16,53 +17,59 @@ public class SYS_USER implements Serializable {
     @Prev(els = {@EL("uuid")})
     @ApiModelProperty("用户id")//用在属性上，对属性做注释
     @Comment("用户id")//定义脚本中添加comment属性来添加注释
-    @Column("user_id")
-    @ColDefine(type = ColType.VARCHAR,width = 128)
-    private String userId;
+    @Column("id")
+    @ColDefine(type = ColType.VARCHAR,width = 64)
+    private String id;
+
+    @ApiModelProperty("头像地址")
+    @Comment("头像地址")
+    @Column("avatar")
+    @ColDefine(type = ColType.VARCHAR,width = 255)
+    private String avatar;
+
+    @ApiModelProperty("创建日期")
+    @Comment("创建日期")
+    @Column("create_time")
+    @ColDefine(type = ColType.DATE)
+    private Date createTime;
 
     @ApiModelProperty("账号")
     @Comment("账号")
     @NotEmpty(message = "请输入用户名!")
-    @Column("user_account")
-    @ColDefine(type = ColType.VARCHAR,width = 128)
-    private String userAccount;
-
-    @ApiModelProperty("姓名")
-    @Comment("姓名")
     @Column("user_name")
-    @ColDefine(type = ColType.VARCHAR,width = 225)
+    @ColDefine(type = ColType.VARCHAR,width = 255)
     private String userName;
 
     @ApiModelProperty("密码")
     @Comment("密码")
     @NotEmpty(message = "请输入密码!")
-    @Column("user_password")
-    @ColDefine(type = ColType.VARCHAR,width = 128)
-    private String userPassword;
-
-    @ApiModelProperty("邮箱")
-    @Comment("邮箱")
-    @Column("user_email")
-    @ColDefine(type = ColType.VARCHAR,width = 128)
-    private String userEmail;
-
-    @ApiModelProperty("编号")
-    @Comment("编号")
-    @Column("user_salt")
-    @ColDefine(type = ColType.VARCHAR,width = 64)
-    private String userSalt;
-
-    @ApiModelProperty("手机")
-    @Comment("手机")
-    @Column("user_phone")
-    @ColDefine(type = ColType.VARCHAR,width = 64)
-    private String userPhone;
-
-    @ApiModelProperty("头像")
-    @Comment("头像")
-    @Column("USER_AVATAR")
+    @Column("password")
     @ColDefine(type = ColType.VARCHAR,width = 255)
-    private String avatar;
+    private String password;
+
+    @ApiModelProperty("最后修改密码的日期")
+    @Comment("最后修改密码的日期")
+    @Column("last_password_reset_time")
+    @ColDefine(type = ColType.DATE)
+    private String lastTime;
+
+    @ApiModelProperty("状态：1启用、0禁用")
+    @Comment("状态：1启用、0禁用")
+    @Column("enabled")
+    @ColDefine(type = ColType.INT)
+    private String enabled;
+
+    @ApiModelProperty("人员ID")
+    @Comment("人员ID")
+    @Column("people_id")
+    @ColDefine(type = ColType.VARCHAR,width = 64)
+    private String peopleId;
+
+    @ApiModelProperty("人员ID")
+    @Comment("人员ID")
+    @Column("unit_id")
+    @ColDefine(type = ColType.VARCHAR,width = 64)
+    private String unitId;
 
     @ApiModelProperty("验证码ID")
     private String uuid;
@@ -73,14 +80,18 @@ public class SYS_USER implements Serializable {
     @ApiModelProperty("用于系统免登陆")
     private String token;
 
-    @ApiModelProperty("用于系统免登陆获取当前系统权限")
-    private String appId;
 
 //    @ApiModelProperty("用户关联所有菜单[已去重]")
 //    private List<SYS_Menu> menus;
 
-    @ApiModelProperty("用户关联角色")
-    private List<SYS_Role> roles;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getAvatar() {
         return avatar;
@@ -90,61 +101,12 @@ public class SYS_USER implements Serializable {
         this.avatar = avatar;
     }
 
-    public List<SYS_Role> getRoles() {
-        return roles;
+    public Date getCreateTime() {
+        return createTime;
     }
 
-    public void setRoles(List<SYS_Role> roles) {
-        this.roles = roles;
-    }
-
-
-    public String getAppId() {
-        return appId;
-    }
-
-    public void setAppId(String appId) {
-        this.appId = appId;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserAccount() {
-        return userAccount;
-    }
-
-    public void setUserAccount(String userAccount) {
-        this.userAccount = userAccount;
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
     }
 
     public String getUserName() {
@@ -155,35 +117,67 @@ public class SYS_USER implements Serializable {
         this.userName = userName;
     }
 
-    public String getUserPassword() {
-        return userPassword;
+    public String getPassword() {
+        return password;
     }
 
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getUserEmail() {
-        return userEmail;
+    public String getLastTime() {
+        return lastTime;
     }
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
+    public void setLastTime(String lastTime) {
+        this.lastTime = lastTime;
     }
 
-    public String getUserSalt() {
-        return userSalt;
+    public String getEnabled() {
+        return enabled;
     }
 
-    public void setUserSalt(String userSalt) {
-        this.userSalt = userSalt;
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
     }
 
-    public String getUserPhone() {
-        return userPhone;
+    public String getPeopleId() {
+        return peopleId;
     }
 
-    public void setUserPhone(String userPhone) {
-        this.userPhone = userPhone;
+    public void setPeopleId(String peopleId) {
+        this.peopleId = peopleId;
+    }
+
+    public String getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(String unitId) {
+        this.unitId = unitId;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
